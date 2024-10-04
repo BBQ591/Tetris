5e948ca9037d799a962032ab0d7e620f9b6a79fb
Brian Qu <bbq@Brians-MacBook-Pro.local>
2024-10-03 21:16:11 -0500

    finish piece class

--- src/main/java/assignment/TetrisPiece.java
+++ src/main/java/assignment/TetrisPiece.java
@@ -1,6 +1,7 @@
 package assignment;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.awt.Point;
 
 import java.awt.*;
 
@@ -15,28 +16,21 @@ import java.awt.*;
 
 
 public final class TetrisPiece implements Piece {
+
     public class Node {
         //        public Node(PieceType type) {
 //            Node next =
 //        }
         Node next;
-        ArrayList<Point> val;
-        Node prev;
+        Point[] val;
         int index;
-        public Node(ArrayList<Point> val, int index) {
+        public Node(Point[] val, int index) {
             this.index = index;
             this.val = val;
             this.next = null;
-            this.prev = null;
         }
-//        public void setLinkedList(PieceType type) {
-//            if (type == Piece.PieceType.T) {
-//                next =
-//            }
-//            return;
-//        }
-
     }
+    PieceType pieceType;
     /**
      * Construct a tetris piece of the given type. The piece should be in its spawn orientation,
      * i.e., a rotation index of 0.
@@ -47,59 +41,56 @@ public final class TetrisPiece implements Piece {
     public Node orientation;
 
     public TetrisPiece(PieceType type) {
-        ArrayList<ArrayList<Point>> arr = new ArrayList<>();
+        this.pieceType = type;
+        ArrayList<Point[]> arr = new ArrayList<>();
         if (type == Piece.PieceType.T) {
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 1), new Point(1,1), new Point(2,1), new Point(1,2))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1, 0), new Point(1,1), new Point(1,2), new Point(2,1))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1, 0), new Point(1,1), new Point(0,1), new Point(2,1))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1,0), new Point(1,1), new Point(1,2), new Point(0,1))));
+            arr.add(new Point[]{new Point(0, 1), new Point(1,1), new Point(2,1), new Point(1,2)});
+            arr.add(new Point[]{new Point(1, 0), new Point(1,1), new Point(1,2), new Point(2,1)});
+            arr.add(new Point[]{new Point(1, 0), new Point(1,1), new Point(0,1), new Point(2,1)});
+            arr.add(new Point[]{new Point(1,0), new Point(1,1), new Point(1,2), new Point(0,1)});
         }
         else if (type == Piece.PieceType.SQUARE) {
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0))));
+            arr.add(new Point[]{new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0)});
+            arr.add(new Point[]{new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0)});
+            arr.add(new Point[]{new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0)});
+            arr.add(new Point[]{new Point(0, 0), new Point(1,1), new Point(0,1), new Point(1,0)});
         }
         else if (type == Piece.PieceType.STICK) {
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 2), new Point(1,2), new Point(2,2), new Point(3,2))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(2,0), new Point(2,1), new Point(2,2), new Point(2,3))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,1), new Point(1,1), new Point(2,1), new Point(3,1))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1,0), new Point(1,1), new Point(1,2), new Point(1,3))));
+            arr.add(new Point[]{new Point(0, 2), new Point(1,2), new Point(2,2), new Point(3,2)});
+            arr.add(new Point[]{new Point(2,0), new Point(2,1), new Point(2,2), new Point(2,3)});
+            arr.add(new Point[]{new Point(0,1), new Point(1,1), new Point(2,1), new Point(3,1)});
+            arr.add(new Point[]{new Point(1,0), new Point(1,1), new Point(1,2), new Point(1,3)});
         }
         else if (type == Piece.PieceType.LEFT_L) {
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 1), new Point(0,2), new Point(1,1), new Point(2,1))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1,0), new Point(1,1), new Point(2,1), new Point(2,2))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,1), new Point(1,1), new Point(2,1), new Point(2,0))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,0), new Point(1,0), new Point(1,1), new Point(1,2))));
+            arr.add(new Point[]{new Point(0, 1), new Point(0,2), new Point(1,1), new Point(2,1)});
+            arr.add(new Point[]{new Point(1,0), new Point(1,1), new Point(2,1), new Point(2,2)});
+            arr.add(new Point[]{new Point(0,1), new Point(1,1), new Point(2,1), new Point(2,0)});
+            arr.add(new Point[]{new Point(0,0), new Point(1,0), new Point(1,1), new Point(1,2)});
         }
         else if (type == Piece.PieceType.RIGHT_L) {
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 1), new Point(0,1), new Point(2,1), new Point(2,2))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1,0), new Point(1,1), new Point(1,2), new Point(2,0))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,0), new Point(0,1), new Point(1,1), new Point(2,1))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1,0), new Point(1,1), new Point(1,2), new Point(0,2))));
+            arr.add(new Point[]{new Point(0, 1), new Point(0,1), new Point(2,1), new Point(2,2)});
+            arr.add(new Point[]{new Point(1,0), new Point(1,1), new Point(1,2), new Point(2,0)});
+            arr.add(new Point[]{new Point(0,0), new Point(0,1), new Point(1,1), new Point(2,1)});
+            arr.add(new Point[]{new Point(1,0), new Point(1,1), new Point(1,2), new Point(0,2)});
         }
         else if (type == Piece.PieceType.LEFT_DOG) {
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0, 2), new Point(1,2), new Point(1,1), new Point(2,1))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1,0), new Point(1,1), new Point(2,1), new Point(2,2))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,1), new Point(1,1), new Point(1,0), new Point(2,0))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,0), new Point(0,1), new Point(1,1), new Point(1,2))));
+            arr.add(new Point[]{new Point(0, 2), new Point(1,2), new Point(1,1), new Point(2,1)});
+            arr.add(new Point[]{new Point(1,0), new Point(1,1), new Point(2,1), new Point(2,2)});
+            arr.add(new Point[]{new Point(0,1), new Point(1,1), new Point(1,0), new Point(2,0)});
+            arr.add(new Point[]{new Point(0,0), new Point(0,1), new Point(1,1), new Point(1,2)});
         }
         else if (type == Piece.PieceType.RIGHT_DOG) {
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,1), new Point(1,1), new Point(1,2), new Point(2,2))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(2,0), new Point(2,1), new Point(1,1), new Point(1,2))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(0,0), new Point(1,0), new Point(1,1), new Point(2,1))));
-            arr.add(new ArrayList<>(Arrays.asList(new Point(1,0), new Point(1,1), new Point(0,1), new Point(0,2))));
+            arr.add(new Point[]{new Point(0,1), new Point(1,1), new Point(1,2), new Point(2,2)});
+            arr.add(new Point[]{new Point(2,0), new Point(2,1), new Point(1,1), new Point(1,2)});
+            arr.add(new Point[]{new Point(0,0), new Point(1,0), new Point(1,1), new Point(2,1)});
+            arr.add(new Point[]{new Point(1,0), new Point(1,1), new Point(0,1), new Point(0,2)});
         }
 
 
         orientation = new Node(null, 0);
         Node pointer = orientation;
-        Node prev = null;
         int index = 0;
         while (index < 4) {
-            if (prev != null) {
-                pointer.prev = prev;
-            }
             pointer.val = arr.get(index);
             pointer.index = index;
             if (index != 3) {
@@ -108,23 +99,15 @@ public final class TetrisPiece implements Piece {
             else {
                 pointer.next = orientation;
             }
-            prev = pointer;
+
             pointer = pointer.next;
             index++;
+
         }
-        if (type == Piece.PieceType.T) {
-            pointer.prev = prev;
-            index = 0;
-            while (index < 4) {
-                System.out.println(pointer.val + "" + pointer.index);
-                pointer = pointer.next;
-                index += 1;
-            }
-        }
-
-
-
-
+        /**
+         * Critter x = new eachCritter();
+         * Piece x = new TetrisPiece;
+         */
 
 //        orientation.setLinkedList(type);
     }
@@ -133,41 +116,94 @@ public final class TetrisPiece implements Piece {
     public PieceType getType() {
         // TODO: Implement me.
         //sets = {[point1, point2, ...], orientation2, orientation3 ...}
-        return null;
+        return this.pieceType;
     }
 
     @Override
     public int getRotationIndex() {
         // TODO: Implement me.
         //linkedlist with each node self.prev, self.next, self.points, self.index
-        return -1;
+        return this.orientation.index;
     }
 
     @Override
     public Piece clockwisePiece() {
         // TODO: Implement me.
         //.next on linked list
-        return null;
+        /**
+         * Piece x = new TetrisPiece(this.getType())
+         * x.setIndex(this.getRotationIndex()+1 % 4)
+         */
+        TetrisPiece newPiece = new TetrisPiece(this.getType());
+        newPiece.setIndex((this.getRotationIndex()+1)%4);
+        return newPiece;
+    }
+
+    public void setIndex(int rotationIndex) {
+        while (this.orientation.index != rotationIndex) {
+            this.orientation = this.orientation.next;
+        }
     }
 
     @Override
     public Piece counterclockwisePiece() {
         // TODO: Implement me.
-        //.prev on linked list
-        return null;
+        TetrisPiece newPiece = new TetrisPiece(this.getType());
+        newPiece.setIndex((this.getRotationIndex()-1)%4);
+        return newPiece;
     }
 
     @Override
     public int getWidth() {
         // TODO: Implement me.
-        //returns the box, regardless of orientation
+        if (this.pieceType == Piece.PieceType.T) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.SQUARE) {
+            return 2;
+        }
+        else if (this.pieceType == Piece.PieceType.STICK) {
+            return 4;
+        }
+        else if (this.pieceType == Piece.PieceType.LEFT_L) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.RIGHT_L) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.LEFT_DOG) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.RIGHT_DOG) {
+            return 3;
+        }
         return -1;
     }
 
     @Override
     public int getHeight() {
         // TODO: Implement me.
-        //same as getWidth
+        if (this.pieceType == Piece.PieceType.T) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.SQUARE) {
+            return 2;
+        }
+        else if (this.pieceType == Piece.PieceType.STICK) {
+            return 4;
+        }
+        else if (this.pieceType == Piece.PieceType.LEFT_L) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.RIGHT_L) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.LEFT_DOG) {
+            return 3;
+        }
+        else if (this.pieceType == Piece.PieceType.RIGHT_DOG) {
+            return 3;
+        }
         return -1;
     }
 
@@ -175,14 +211,18 @@ public final class TetrisPiece implements Piece {
     public Point[] getBody() {
         // TODO: Implement me.
         // return whatever is in current linked list node
-        return null;
+        return this.orientation.val;
     }
 
     @Override
     public int[] getSkirt() {
         // TODO: Implement me.
         // returns the lowest value at each x axis
-        return null;
+        int[] skirt = new int[this.getHeight()];
+        for (int i = 0; i < this.orientation.val.length; i++) {
+            skirt[this.orientation.val[i].x] = Math.min(skirt[this.orientation.val[i].x], this.orientation.val[i].y);
+        }
+        return skirt;
     }
 
     @Override
@@ -192,7 +232,10 @@ public final class TetrisPiece implements Piece {
         TetrisPiece otherPiece = (TetrisPiece) other;
 
         // TODO: Implement me.
-        return false;
+        for (int i = 0; i < this.orientation.val.length; i++) {
+            if (this.orientation.val[i].x != otherPiece.orientation.val[i].x || this.orientation.val[i].y != otherPiece.orientation.val[i].y) return false;
+        }
+        return true;
         //verify that the points are all the same
     }
 }
